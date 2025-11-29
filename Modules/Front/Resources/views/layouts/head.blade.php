5314a1c719ddeaa5cf1873591d63f31cad099a81
maziardehghani <maziardehghani1380@gmail.com>
2025-11-29 07:24:53 +0330

    dafds

--- Modules/Front/Resources/views/layouts/head.blade.php
+++ Modules/Front/Resources/views/layouts/head.blade.php
@@ -1,24 +1,33 @@
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0;">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>webamooz</title>
+    <title>Webamooz</title>
+
     <meta name="description"
-          content="وب آموز وبسایت آموزش برنامه نویسی وب و موبایل ، جاوااسکریپت ، لاراول ، react ، آموزش node js با مجرب ترین مدرسین">
+          content="Webamooz is a programming education website for web and mobile development, including JavaScript, Laravel, React, and Node.js courses provided by professional instructors.">
+
     <meta name="keywords"
-          content="آموزش طراحی سایت,آموزش برنامه نویسی,طراحی وب,ساخت وب سایت,آموزش git,آموزش لاراول,آموزش php,آموزش react,آموزش پی اچ پی,آموزش laravel,آموزش جاوا اسکریپت,آموزش ساخت وب سایت,آموزش mvc,آموزش React Native,وب آموز , وب اموز">
+          content="web design tutorial, programming tutorial, web development, build website, git tutorial, laravel tutorial, php tutorial, react tutorial, javascript tutorial, node.js tutorial, react native tutorial, mvc tutorial, webamooz">
+
     <link rel="canonical" href="https://webamooz.ir"/>
-    <meta property="og:title" content="وب آموز | آموزش برنامه‌ نویسی و طراحی وب"/>
+
+    <meta property="og:title" content="Webamooz | Programming & Web Design Tutorials"/>
+
     <meta property="og:description"
-          content="وب آموز وبسایت آموزش برنامه نویسی وب و موبایل ، جاوااسکریپت ، لاراول ، react ، آموزش node js با مجرب ترین مدرسین"/>
+          content="Webamooz is an online learning platform for web and mobile development, including Laravel, JavaScript, React, and Node.js courses.">
+
     <meta property="og:url" content="https://webamooz.ir"/>
-    <meta property="og:site_name" content="وبسایت آموزشی وب آموز"/>
-    <meta property="og:brand" content="وب آموز"/>
-    <meta property="og:locale" content="en"/>
+
+    <meta property="og:site_name" content="Webamooz Learning Platform"/>
+
+    <meta property="og:brand" content="Webamooz"/>
+
+    <meta property="og:locale" content="en_US"/>
+
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link rel="stylesheet" href="{{asset('css/font/font.css')}}">
     <link rel="stylesheet" href="{{asset('css/responsive.css')}}" media="(max-width:991px)">
     <link rel="stylesheet" href="{{asset('css/modal.css')}}">
-
 </head>
